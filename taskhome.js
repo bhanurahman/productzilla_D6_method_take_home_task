cacf1b3eb3dafadf140e2168fb5290d0ad9ee1c6
Aryosatyo Bhanurahman <bhanurahman919@gmail.com>
2023-03-04 16:57:18 +0700

    inisiasi Project

---
+++ taskhome.js
@@ -0,0 +1,221 @@
+console.log('====','Buatlah 20 Procedure & function','====')
+let nama;
+function setNama(argNama1) {
+    nama = argNama1
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama1')
+    console.log(nama)
+getNama()
+
+function setNama(argNama2) {
+    nama = argNama2
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama2')
+    console.log(nama)
+getNama()
+
+function setNama(argNama3) {
+    nama = argNama3
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama3')
+    console.log(nama)
+getNama()
+
+function setNama(argNama4) {
+    nama = argNama4
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama4')
+    console.log(nama)
+getNama()
+
+function setNama(argNama5) {
+    nama = argNama5
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama5')
+    console.log(nama)
+getNama()
+
+function setNama(argNama6) {
+    nama = argNama6
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama6')
+    console.log(nama)
+getNama()
+
+function setNama(argNama7) {
+    nama = argNama7
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama7')
+    console.log(nama)
+getNama()
+
+function setNama(argNama8) {
+    nama = argNama8
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama8')
+    console.log(nama)
+getNama()
+
+function setNama(argNama9) {
+    nama = argNama9
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama9')
+    console.log(nama)
+getNama()
+
+function setNama(argNama10) {
+    nama = argNama10
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama10')
+    console.log(nama)
+getNama()
+
+function setNama(argnama11) {
+    nama = argnama11
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argnama11')
+    console.log(nama)
+getNama()
+
+function setNama(argNama12) {
+    nama = argNama12
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama12')
+    console.log(nama)
+getNama()
+
+function setNama(argNama13) {
+    nama = argNama13
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama13')
+    console.log(nama)
+getNama()
+
+function setNama(argNama14) {
+    nama = argNama14
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama14')
+    console.log(nama)
+getNama()
+
+function setNama(argNama15) {
+    nama = argNama15
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama15')
+    console.log(nama)
+getNama()
+
+function setNama(argNama16) {
+    nama = argNama16
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama16')
+    console.log(nama)
+getNama()
+
+function setNama(argNama17) {
+    nama = argNama17
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama17')
+    console.log(nama)
+getNama()
+
+function setNama(argNama18) {
+    nama = argNama18
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama18')
+    console.log(nama)
+getNama()
+
+function setNama(argNama19) {
+    nama = argNama19
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama19')
+    console.log(nama)
+getNama()
+
+function setNama(argNama20) {
+    nama = argNama20
+}
+function getNama() {
+
+    return nama
+}
+    setNama('argNama20')
+    console.log(nama)
+getNama()
